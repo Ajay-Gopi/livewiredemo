a10d00bbd07076f9cd79ea9a2aa1ca4f745d08d0
livewire <nibest18@gmail.com>
2019-09-18 18:33:14 +0530

    first-commit

---
+++ todo/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from todo.views import todoadd,todoedit,tododelete,todolist
+
+
+urlpatterns = [
+    url(r'^$',todolist,name='todolist'),
+    url(r'^add/$',todoadd,name='todoadd'),
+    url(r'^edit/(?P<pk>\d+)$',todoedit,name='todoedit'),
+    url(r'^delete/(?P<pk>\d+)$',tododelete,name='tododelete'),
+    ]
